4e4dc2d8c9f8bb3e295164939a190b723c67c85b
Michael Adragna <michael.adragna1@gmail.com>
2022-10-01 18:43:00 -0400

    spacing

--- hand_detection.py
+++ hand_detection.py
@@ -25,6 +25,7 @@ class Controller:
 		self.right_bumper = 0
 		self.joystick = 0
 		self.Abutton = 0
+		self.player = 0
 	def __add__(self, other):
 		ret = Controller()
 		ret.left_trigger = self.left_trigger + other.left_trigger
@@ -55,77 +56,77 @@ class Controller:
 
 
 def getDelta(newCoords, oldCoords):
-    deltaArray = [] # array of delta coords
-    deltaAverageX = 0
-    deltaAverageY = 0
-    deltaAverageZ = 0
-    deltaAverage = 0
+	deltaArray = [] # array of delta coords
+	deltaAverageX = 0
+	deltaAverageY = 0
+	deltaAverageZ = 0
+	deltaAverage = 0
 
-    if (oldCoords == [] or newCoords == [] or oldCoords == None or newCoords == None): # base case if we dont have previous hand coords
-        deltaArray.append(100)
-        return 2
-    else: # Regular case where we take new coords and subtract from prev coords
-        for i in range(len(newCoords)):
-            for j in range(len(newCoords[i].landmark)):
-                print("--------------------")
-                deltax = abs(newCoords[i].landmark[j].x - oldCoords[i].landmark[j].x)
-                deltay = abs(newCoords[i].landmark[j].y - oldCoords[i].landmark[j].y)
-                deltaz = abs(newCoords[i].landmark[j].z - oldCoords[i].landmark[j].z)
-                deltaArray.append({"deltax": deltax, "deltay": deltay, "deltaz": deltaz})
+	if (oldCoords == [] or newCoords == [] or oldCoords == None or newCoords == None): # base case if we dont have previous hand coords
+		deltaArray.append(100)
+		return 2
+	else: # Regular case where we take new coords and subtract from prev coords
+		for i in range(len(newCoords)):
+			for j in range(len(newCoords[i].landmark)):
+				print("--------------------")
+				deltax = abs(newCoords[i].landmark[j].x - oldCoords[i].landmark[j].x)
+				deltay = abs(newCoords[i].landmark[j].y - oldCoords[i].landmark[j].y)
+				deltaz = abs(newCoords[i].landmark[j].z - oldCoords[i].landmark[j].z)
+				deltaArray.append({"deltax": deltax, "deltay": deltay, "deltaz": deltaz})
 
-    for i in deltaArray:
-        print(i)
-        deltaAverageX += i["deltax"]
-        deltaAverageY += i["deltay"]
-        deltaAverageZ += i["deltaz"]
+	for i in deltaArray:
+		print(i)
+		deltaAverageX += i["deltax"]
+		deltaAverageY += i["deltay"]
+		deltaAverageZ += i["deltaz"]
 
-    deltaAverageX /= float(len(deltaArray))
-    deltaAverageY /= float(len(deltaArray))
-    deltaAverageZ /= float(len(deltaArray))
+	deltaAverageX /= float(len(deltaArray))
+	deltaAverageY /= float(len(deltaArray))
+	deltaAverageZ /= float(len(deltaArray))
 
-    deltaAverage = (deltaAverageX + deltaAverageY + deltaAverageZ) / 3.0
+	deltaAverage = (deltaAverageX + deltaAverageY + deltaAverageZ) / 3.0
 
-    print(deltaAverage)
-    return deltaAverage
+	print(deltaAverage)
+	return deltaAverage
 
 def checkButtonPress(movementDiff, calibratedMovementDiff, movementRatio):
-    if (movementDiff < calibratedMovementDiff * movementRatio):
-        return 1
-    else:
-        return 0
+	if (movementDiff < calibratedMovementDiff * movementRatio):
+		return 1
+	else:
+		return 0
 
 
 def xDiff(curCoords, nailNumber, nuckleNumber, handNum):
-    if (curCoords and curCoords[handNum]):
-        nuckleDiff = abs(curCoords[handNum].landmark[17].y - curCoords[handNum].landmark[5].y)
-        calibratednuckleDiff =  abs(calibratedCoords[handNum].landmark[17].y - calibratedCoords[handNum].landmark[5].y)
-        movementDiff = abs(curCoords[handNum].landmark[nailNumber].x - curCoords[handNum].landmark[nuckleNumber].x)
-        calibratedMovementDiff = abs(calibratedCoords[handNum].landmark[nailNumber].x - calibratedCoords[handNum].landmark[nuckleNumber].x) * nuckleDiff/calibratednuckleDiff
+	if (curCoords and curCoords[handNum]):
+		nuckleDiff = abs(curCoords[handNum].landmark[17].y - curCoords[handNum].landmark[5].y)
+		calibratednuckleDiff =  abs(calibratedCoords[handNum].landmark[17].y - calibratedCoords[handNum].landmark[5].y)
+		movementDiff = abs(curCoords[handNum].landmark[nailNumber].x - curCoords[handNum].landmark[nuckleNumber].x)
+		calibratedMovementDiff = abs(calibratedCoords[handNum].landmark[nailNumber].x - calibratedCoords[handNum].landmark[nuckleNumber].x) * nuckleDiff/calibratednuckleDiff
 
-        return (movementDiff, calibratedMovementDiff)
-    else:
-        return (0,0)
+		return (movementDiff, calibratedMovementDiff)
+	else:
+		return (0,0)
 
 def yDiff(curCoords, nailNumber, nuckleNumber, handNum):
-    if (curCoords and curCoords[handNum]):
-        nuckleDiff = abs(curCoords[handNum].landmark[17].y - curCoords[handNum].landmark[5].y)
-        calibratednuckleDiff =  abs(calibratedCoords[handNum].landmark[17].y - calibratedCoords[handNum].landmark[5].y)
-        movementDiff = abs(curCoords[handNum].landmark[nailNumber].y - curCoords[handNum].landmark[nuckleNumber].y)
-        calibratedMovementDiff = abs(calibratedCoords[handNum].landmark[nailNumber].y - calibratedCoords[handNum].landmark[nuckleNumber].y) * nuckleDiff/calibratednuckleDiff
+	if (curCoords and curCoords[handNum]):
+		nuckleDiff = abs(curCoords[handNum].landmark[17].y - curCoords[handNum].landmark[5].y)
+		calibratednuckleDiff =  abs(calibratedCoords[handNum].landmark[17].y - calibratedCoords[handNum].landmark[5].y)
+		movementDiff = abs(curCoords[handNum].landmark[nailNumber].y - curCoords[handNum].landmark[nuckleNumber].y)
+		calibratedMovementDiff = abs(calibratedCoords[handNum].landmark[nailNumber].y - calibratedCoords[handNum].landmark[nuckleNumber].y) * nuckleDiff/calibratednuckleDiff
 
-        return (movementDiff, calibratedMovementDiff)
-    else:
-        return (0,0)
+		return (movementDiff, calibratedMovementDiff)
+	else:
+		return (0,0)
 
 def triggerPosition(movementDiff, calibratedMovementDiff):
-    if(not movementDiff):
-        return None
-    ratio = movementDiff/calibratedMovementDiff
-    ratio -= 1
-    ratio /= 0.75 # scaling param that has yet to be determined
-    ratio = max(ratio, -1) # bound the joystick between -1 and 1
-    ratio = min(ratio, 1)
-    return ratio
+	if(not movementDiff):
+		return None
+	ratio = movementDiff/calibratedMovementDiff
+	ratio -= 1
+	ratio /= 0.75 # scaling param that has yet to be determined
+	ratio = max(ratio, -1) # bound the joystick between -1 and 1
+	ratio = min(ratio, 1)
+	return ratio
 
 C = Controller()
 temp_controllers = [Controller(), Controller(), Controller()]
@@ -134,115 +135,116 @@ cap = cv2.VideoCapture(0)
 startTime = datetime.datetime.now()
 counter = 0
 with mp_hands.Hands(
-    model_complexity=1,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.7) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+	model_complexity=1,
+	min_detection_confidence=0.5,
+	min_tracking_confidence=0.7) as hands:
+	while cap.isOpened():
+		success, image = cap.read()
+		if not success:
+			print("Ignoring empty camera frame.")
+			# If loading a video, use 'break' instead of 'continue'.
+			continue
 
-    counter += 1
+		counter += 1
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+		# To improve performance, optionally mark the image as not writeable to
+		# pass by reference.
+		image.flags.writeable = False
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		results = hands.process(image)
 
-    # results.multi_hand_landmark stores each hand landmark x,y,z
-    # do calibration
+		# results.multi_hand_landmark stores each hand landmark x,y,z
+		# do calibration
 
-    curTime = datetime.datetime.now()
-    if (2 < (curTime - startTime).total_seconds() < 3):
-        calibratedCoords = results.multi_hand_landmarks
-        print("calibration complete")
+		curTime = datetime.datetime.now()
+		if (2 < (curTime - startTime).total_seconds() < 3):
+			calibratedCoords = results.multi_hand_landmarks
+			print("calibration complete")
 
-    if (calibratedCoords != None and len(calibratedCoords) > 0):
-        if results.multi_handedness != None:
-            for i in range(len(results.multi_handedness)):
-                if (results.multi_handedness[i].classification[0].label == "Left"):
-                    (rt1Movementdiff, rt1CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 8, 5, i)
-                    (rt2Movementdiff, rt2CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 12, 9, i)
-                    (aMovementdiff, aCalibratedMovementDiff) = yDiff(results.multi_hand_landmarks, 4, 5, i)
+		if (calibratedCoords != None and len(calibratedCoords) > 0):
+			if results.multi_handedness != None:
+				for i in range(len(results.multi_handedness)):
+					if (results.multi_handedness[i].classification[0].label == "Left"):
 
-
-                    rt1ButtonPressed = checkButtonPress(rt1Movementdiff, rt1CalibratedMovementDiff, 0.85)
-                    rt2ButtonPressed = checkButtonPress(rt2Movementdiff, rt2CalibratedMovementDiff, 0.87)
-                    aButtonPressed = checkButtonPress(aMovementdiff, aCalibratedMovementDiff, 0.90)
-
-                    temp_controllers[counter%3].right_trigger = rt1ButtonPressed
-                    temp_controllers[counter%3].right_bumper = rt2ButtonPressed
-                    temp_controllers[counter%3].Abutton = aButtonPressed
+						(rt1Movementdiff, rt1CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 8, 5, i)
+						(rt2Movementdiff, rt2CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 12, 9, i)
+						(aMovementdiff, aCalibratedMovementDiff) = yDiff(results.multi_hand_landmarks, 4, 5, i)
 
 
-                    if (rt1ButtonPressed == 1):
-                        rt1Button +=1
-                        if(rt1Button < 2):
-                            print("R1 Pressed")
-                    else:
-                        rt1Button = 0
+						rt1ButtonPressed = checkButtonPress(rt1Movementdiff, rt1CalibratedMovementDiff, 0.85)
+						rt2ButtonPressed = checkButtonPress(rt2Movementdiff, rt2CalibratedMovementDiff, 0.87)
+						aButtonPressed = checkButtonPress(aMovementdiff, aCalibratedMovementDiff, 0.90)
 
-                    if (rt2ButtonPressed == 1):
-                        rt2Button += 1
-                        if(rt2Button < 2):
-                            print("R2 Pressed")
-                    else:
-                        rt2Button = 0
+						temp_controllers[counter%3].right_trigger = rt1ButtonPressed
+						temp_controllers[counter%3].right_bumper = rt2ButtonPressed
+						temp_controllers[counter%3].Abutton = aButtonPressed
 
-                    if (aButtonPressed == 1):
-                        aButton += 1
-                        if(aButton < 2):
-                            print("A Pressed")
-                    else:
-                        aButton = 0
 
-                elif (results.multi_handedness[i].classification[0].label == "Right"):
-                    (lt1Movementdiff, lt1CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 8, 5, i)
-                    (lt2Movementdiff, lt2CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 12, 9, i)
+						if (rt1ButtonPressed == 1):
+							rt1Button +=1
+							if(rt1Button < 2):
+								print("R1 Pressed")
+						else:
+							rt1Button = 0
 
-                    lt1ButtonPressed = checkButtonPress(lt1Movementdiff, lt1CalibratedMovementDiff, 0.90)
-                    lt2ButtonPressed = checkButtonPress(lt2Movementdiff, lt2CalibratedMovementDiff, 0.90)
+						if (rt2ButtonPressed == 1):
+							rt2Button += 1
+							if(rt2Button < 2):
+								print("R2 Pressed")
+						else:
+							rt2Button = 0
 
-                    temp_controllers[counter%3].left_trigger = lt1ButtonPressed
-                    temp_controllers[counter%3].left_bumper = lt2ButtonPressed
+						if (aButtonPressed == 1):
+							aButton += 1
+							if(aButton < 2):
+								print("A Pressed")
+						else:
+							aButton = 0
 
-                    m, c = xDiff(results.multi_hand_landmarks, 4, 5, i)
-                    joystickPosition = triggerPosition(m,c)
-                    temp_controllers[counter%3].joystick = joystickPosition
-                    if (counter%3 == 0):
-                        C = (temp_controllers[0] + temp_controllers[1] + temp_controllers[2])/3
-                        print(C)
-                        
-                    if (lt1ButtonPressed == 1):
-                        lt1Button += 1
-                        if(lt1Button < 2):
-                            print("L1 Pressed")
-                    else:
-                        lt1Button = 0 
+					elif (results.multi_handedness[i].classification[0].label == "Right"):
+						(lt1Movementdiff, lt1CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 8, 5, i)
+						(lt2Movementdiff, lt2CalibratedMovementDiff) = xDiff(results.multi_hand_landmarks, 12, 9, i)
 
-                    if (lt2ButtonPressed == 1):
-                        lt2Button += 1
-                        if(lt2Button < 2):
-                            print("A Pressed")
-                    else:
-                        lt2Button = 0                                          
+						lt1ButtonPressed = checkButtonPress(lt1Movementdiff, lt1CalibratedMovementDiff, 0.90)
+						lt2ButtonPressed = checkButtonPress(lt2Movementdiff, lt2CalibratedMovementDiff, 0.90)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-        break
+						temp_controllers[counter%3].left_trigger = lt1ButtonPressed
+						temp_controllers[counter%3].left_bumper = lt2ButtonPressed
+
+						m, c = xDiff(results.multi_hand_landmarks, 4, 5, i)
+						joystickPosition = triggerPosition(m,c)
+						temp_controllers[counter%3].joystick = joystickPosition
+						if (counter%3 == 0):
+							C = (temp_controllers[0] + temp_controllers[1] + temp_controllers[2])/3
+							print(C)
+
+						if (lt1ButtonPressed == 1):
+							lt1Button += 1
+							if(lt1Button < 2):
+								print("L1 Pressed")
+						else:
+							lt1Button = 0
+
+						if (lt2ButtonPressed == 1):
+							lt2Button += 1
+							if(lt2Button < 2):
+								print("A Pressed")
+						else:
+							lt2Button = 0
+
+		# Draw the hand annotations on the image.
+		image.flags.writeable = True
+		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+		if results.multi_hand_landmarks:
+			for hand_landmarks in results.multi_hand_landmarks:
+				mp_drawing.draw_landmarks(
+					image,
+					hand_landmarks,
+					mp_hands.HAND_CONNECTIONS,
+					mp_drawing_styles.get_default_hand_landmarks_style(),
+					mp_drawing_styles.get_default_hand_connections_style())
+		# Flip the image horizontally for a selfie-view display.
+		cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+		if cv2.waitKey(5) & 0xFF == 27:
+			break
 cap.release()
